56a761662136c42c43b7647ef7790598e2a192b4
pats07pk <07pawan1988@gmail.com>
2019-04-27 22:09:07 +0530

    Initial commit

---
+++ customerpi/src/main/java/com/cust/pi/CustomerpiApplication.java
@@ -0,0 +1,13 @@
+package com.cust.pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerpiApplication.class, args);
+	}
+
+}
